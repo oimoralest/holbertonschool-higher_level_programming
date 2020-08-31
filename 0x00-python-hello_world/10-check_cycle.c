b31b6c1526b786647d4799f9c877153c84122930
oimoralest <oimoralest@gmail.com>
2020-08-31 14:49:57 -0500

    Merged if and else if statement and modified the return value

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,7 +2,7 @@
 /**
  * check_cycle - check if a singly linked list has a cycle in it
  * @list: head of the list
- * Return: 0 if there is no cycle, 1 if there is a cycle, -1 on Error
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
