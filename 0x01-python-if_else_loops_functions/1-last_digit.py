bbbea98a479e5fdfeb9bc0511dd6158901681d03
oimoralest <oimoralest@gmail.com>
2020-09-01 13:11:42 -0500

    modified print format for last digit of negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,14 @@ if number < 0:
         flag = -1
         number *= -1
 if (number % 10) > 5:
-        print("The last digit of", number * flag, "is", number % 10, "and is \
-greater than 5")
+        print(
+            "The last digit of", number * flag, "is", (number % 10) * flag,
+            "and is greater than 5")
 elif (number % 10) == 0:
-        print("The last digit of", number * flag, "is", number % 10, "and is \
-0")
+        print(
+            "The last digit of", number * flag, "is", (number % 10) * flag,
+            "and is 0")
 else:
-        print("The last digit of", number * flag, "is", number % 10, "and is \
-less than 6 and not 0")
+        print(
+            "The last digit of", number * flag, "is", (number % 10) * flag,
+            "and is less than 6 and not 0")
