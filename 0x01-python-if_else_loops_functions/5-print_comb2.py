07a02765a026ea035d1afeab6117e23c3e7265f6
oimoralest <oimoralest@gmail.com>
2020-09-01 16:44:51 -0500

    Added a function that prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 99:
-        if i < 10:
-            i = '0' + str(i)
-        print("{}".format(i), end=', ')
+        print("{:02}".format(i), end=', ')
     else:
         print("{}".format(i))
