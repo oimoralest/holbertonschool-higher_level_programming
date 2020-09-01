b55a06d5d49dd037631507836e49d629a311ccd0
oimoralest <oimoralest@gmail.com>
2020-09-01 13:24:31 -0500

    Modified error when the absolute value of negative number was greater than 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,14 +4,14 @@ number = random.randint(-10000, 10000)
 flag = 1
 if number < 0:
         flag = -1
-ld = int((number * flag) % 10)
+ld = int((number * flag) % 10) * flag
 if ld > 5:
         print(
-            "Last digit of", number, "is", ld * flag, "and is greater than 5")
+            "Last digit of", number, "is", ld, "and is greater than 5")
 elif ld == 0:
         print(
-            "Last digit of", number, "is", ld * flag, "and is 0")
+            "Last digit of", number, "is", ld, "and is 0")
 else:
         print(
-            "Last digit of", number, "is", ld * flag, "and is less than 6 and \
+            "Last digit of", number, "is", ld, "and is less than 6 and \
 not 0")
