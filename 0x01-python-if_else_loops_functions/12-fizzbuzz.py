4575ef41c8d44c1656b08f80f378b416028e5d36
oimoralest <oimoralest@gmail.com>
2020-09-01 18:17:37 -0500

    Added

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                print("FizzBuzz", end=' ')
+            else:
+                print("Fizz", end=' ')
+        elif i % 5 == 0:
+            if i == 100:
+                print("Buzz", end='')
+            else:
+                print("Buzz", end=' ')
+        else:
+            print(i, end=' ')
