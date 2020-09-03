354da498c00365f98034d06c4e5acbce62153b12
oimoralest <oimoralest@gmail.com>
2020-09-03 12:46:38 -0500

    Modified with pep8 style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         print("{0} arguments.".format(len(argv) - 1))
     else:
         print("{0} {1}:".format(
-                            len(argv) - 1, "argument" if len(argv) - 1 == 1 
+                            len(argv) - 1, "argument" if len(argv) - 1 == 1
                             else "arguments"))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
