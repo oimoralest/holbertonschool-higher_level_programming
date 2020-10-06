d2cdc633fd7e288925885997747fdd83bea3138e
oimoralest <oimoralest@gmail.com>
2020-10-06 18:17:40 -0500

    Fixed docstring for the class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,6 +6,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """
+    This class defines a Square
+    """
     def __init__(self, size):
         """
         Initializes a Square
