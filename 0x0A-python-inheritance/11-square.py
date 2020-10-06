e9945638a4a4258c408e8c6a77927315d1bc8f85
oimoralest <oimoralest@gmail.com>
2020-10-06 18:13:20 -0500

    Fixed import position: PEP 8

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """
 This module defines a class Square
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
