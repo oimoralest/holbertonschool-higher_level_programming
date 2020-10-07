4e4a6bb5b93bb35f7935f06b6d5b758ea3572b6a
oimoralest <oimoralest@gmail.com>
2020-10-07 17:06:49 -0500

    Added encoding for open

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -9,5 +9,5 @@ def save_to_json_file(my_obj, filename):
     """
     writes an Object to a text file, using a JSON representation
     """
-    with open(filename, mode="w") as file:
+    with open(filename, mode="w", encoding="utf-8") as file:
         json.dump(my_obj, file)
