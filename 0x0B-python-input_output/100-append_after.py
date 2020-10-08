80afb0565ba89f3c2407470eb88e77364b617951
oimoralest <oimoralest@gmail.com>
2020-10-07 19:14:38 -0500

    modified

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,10 +11,10 @@ def append_after(filename="", search_string="", new_string=""):
     """
     if type(search_string) is str and search_string != "" \
        and type(new_string) is str:
-        with open(filename, mode="r") as file:
+        with open(filename, mode="r", encoding="utf-8") as file:
             _list = file.readlines()
             for i in _list:
                 if search_string in i:
                     _list.insert(_list.index(i) + 1, new_string)
-        with open(filename, mode="w") as file:
+        with open(filename, mode="w", encoding="utf-8") as file:
             file.writelines(_list)
