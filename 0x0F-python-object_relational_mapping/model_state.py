ec663e5f0e86be00d89923c1b8a9f4acb130cf49
oimoralest <oimoralest@gmail.com>
2020-12-12 13:13:43 -0500

    Modified for PEP8 requirements

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,4 +9,4 @@ class State(Base):
     """States class"""
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
