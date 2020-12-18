9b7997e7cd524c727b9d682c2ec19c4c21787216
oimoralest <oimoralest@gmail.com>
2020-12-18 18:08:17 -0500

    Fixed to use the real curl

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curk -d "@$2" -H 'Content-Type: application/json' "$1"
+curl -d "@$2" -H 'Content-Type: application/json' "$1"
