a09edcffb53e818a91e3c1f4e6094dbd04c4efc2
oimoralest <oimoralest@gmail.com>
2020-12-18 13:11:01 -0500

    Deleted unnecessary grep command

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep 'Allow: ' | awk '{$1=""; sub(/^[ \t ]+/, ""); print $0}'
+curl -sI "$1" | awk '$0 ~ /Allow/ {$1=""; sub(/^[ \t ]+/, ""); print $0}'
