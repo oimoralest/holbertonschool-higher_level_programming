e22c5980920b3ec6ccc5ca5b4a1d4640f47c3bde
oimoralest <oimoralest@gmail.com>
2021-01-11 15:49:26 -0500

    Added  script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This script makes a request to an URL"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:\n"
+              "\t-type: {}\n "
+              "\t-content: {}\n "
+              "\t-utf8 content: {}".format(
+                  type(html), html, html.decode('utf-8')))
