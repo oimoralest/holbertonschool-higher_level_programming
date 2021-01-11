418cd907de8adc2e19e2efd8f61675f74910321d
oimoralest <oimoralest@gmail.com>
2021-01-11 15:58:47 -0500

    Changed print format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """This script makes a request to an URL"""
+import urllib.request
 
 
 if __name__ == '__main__':
-    import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:\n"
-              "\t- type: {}\n "
-              "\t- content: {}\n "
-              "\t- utf8 content: {}".format(
-                  type(html), html, html.decode('utf-8')))
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('utf-8'))
