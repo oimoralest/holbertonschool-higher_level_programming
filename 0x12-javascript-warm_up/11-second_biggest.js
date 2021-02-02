11bea9871368b542116109284a7a8aa1b0d9ae5c
oimoralest <oimoralest@gmail.com>
2021-02-02 13:25:33 -0500

    Added semicolon in line 5

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const len = process.argv.length - 2;
 const data = process.argv.slice(2);
 if (len <= 1) {
-  console.log('0')
+  console.log('0');
 } else {
   data.sort((a, b) => {
     return a - b;
