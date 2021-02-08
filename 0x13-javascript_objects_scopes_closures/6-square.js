dfe28b7c7e2d87b5a21320939cff9838b77d2c80
oimoralest <oimoralest@gmail.com>
2021-02-08 12:54:42 -0500

    Added Class Square that defines a square and inherits from Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const square = require('./5-square');
+
+class Square extends square {
+  charPrint (c = undefined) {
+    if (c) {
+      for (let i = 0; i < this.width; i++) {
+        console.log(`${c}`.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;
